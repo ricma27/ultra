8ba8a3042a14b3607dac37cf6ca90a366719ac47
ricma27 <ricma@hotmail.ch>
2018-06-12 15:52:06 +0200

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-var sonosLireMp3 = require('./lib/sonosLireMp3.js');
-var sonosLireGoogleSpeak = require('./lib/sonosLireGoogleSpeak.js');
-var sonosGetPlaylistAleatoire = require('./lib/sonosGetPlaylistAleatoire.js');
-var sonosLirePlaylist = require('./lib/sonosLirePlaylist.js');
-var hueAllumage = require('./lib/hueAllumage.js');
-var hueAllumageDelai = require('./lib/hueAllumageDelai.js');
-var hueClignotement = require('./lib/hueClignotement.js');
-var hueLuminositeProgressive = require('./lib/hueLuminositeProgressive.js');
-var hueIntensiteRegressive = require('./lib/hueIntensiteRegressive.js');
+var sonosPlayMP3 = require('./lib/sonosPlayMP3.js');
 var install = require('./lib/install.js');
 var uninstall = require('./lib/uninstall.js');
 
 module.exports = function(sails) {
-
 	return {
-		sonosLireMp3: sonosLireMp3,
-		sonosLireGoogleSpeak: sonosLireGoogleSpeak,
-		sonosGetPlaylistAleatoire: sonosGetPlaylistAleatoire,
-		sonosLirePlaylist: sonosLirePlaylist,
-		hueAllumage: hueAllumage,
-		hueAllumageDelai: hueAllumageDelai,
-		hueClignotement: hueClignotement,
-		hueLuminositeProgressive: hueLuminositeProgressive,
-		hueIntensiteRegressive: hueIntensiteRegressive,
+		sonosPlayMP3: sonosPlayMP3,
 		install: install,
 		uninstall: uninstall
 	};
