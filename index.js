ec902a49044935e5ddc7724804f85c47aa0c0cac
ricma27 <ricma@hotmail.ch>
2018-06-13 16:29:32 +0200

    add sonosGoogleSpeak,js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+var sonosGoogleSpeak = require('./lib/sonosGoogleSpeak.js');
 var sonosPlayMP3 = require('./lib/sonosPlayMP3.js');
 var install = require('./lib/install.js');
 var uninstall = require('./lib/uninstall.js');
 
 module.exports = function(sails) {
 	return {
+		sonosGoogleSpeak: sonosGoogleSpeak,		
 		sonosPlayMP3: sonosPlayMP3,
 		install: install,
 		uninstall: uninstall
