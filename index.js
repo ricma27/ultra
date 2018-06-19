111e8db45e63fe41ec4fb3e97caa319271708ce0
ricma27 <ricma@hotmail.ch>
2018-06-19 14:33:21 +0200

    add function sonosPlayRandomPlaylist

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var sonosGetRandomPlaylist = require('./lib/sonosGetRandomPlaylist.js');
-var sonosGoogleSpeak = require('./lib/sonosGoogleSpeak.js');
+var sonosPlayGoogleSpeak = require('./lib/sonosPlayGoogleSpeak.js');
 var sonosPlayMP3 = require('./lib/sonosPlayMP3.js');
 var sonosPlayPlaylist = require('./lib/sonosPlayPlaylist.js');
+var sonosPlayRandomPlaylist = require('./lib/sonosPlayRandomPlaylist.js');
 var install = require('./lib/install.js');
 var uninstall = require('./lib/uninstall.js');
 
 module.exports = function(sails) {
 	return {
 		sonosGetRandomPlaylist: sonosGetRandomPlaylist,	
-		sonosGoogleSpeak: sonosGoogleSpeak,		
+		sonosPlayGoogleSpeak: sonosPlayGoogleSpeak,		
 		sonosPlayMP3: sonosPlayMP3,
 		sonosPlayPlaylist: sonosPlayPlaylist,
+		sonosPlayRandomPlaylist: sonosPlayRandomPlaylist,		
 		install: install,
 		uninstall: uninstall
 	};
